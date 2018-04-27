0a23362ec8717a46db0911114b9cfdee681e8cae
Phil Howard <phil@gadgetoid.com>
2018-04-27 14:54:16 +0100

    Tweaks, improvements, icons, so wow!

--- scrollbit.ts
+++ scrollbit.ts
@@ -1,32 +1,3 @@
-namespace smbus {
-    export function writeByte(addr: number, register: number, value: number): void {
-        let temp = pins.createBuffer(2);
-        temp[0] = register;
-        temp[1] = value;
-        pins.i2cWriteBuffer(addr, temp, false);
-    }
-    export function writeBuffer(addr: number, register: number, value: Buffer): void {
-        let temp = pins.createBuffer(value.length + 1);
-        temp[0] = register;
-        for (let x = 0; x < value.length; x++) {
-            temp[x + 1] = value[x];
-        }
-        pins.i2cWriteBuffer(addr, temp, false);
-    }
-    export function readBuffer(addr: number, register: number, len: number): Buffer {
-        let temp = pins.createBuffer(1);
-        temp[0] = register;
-        pins.i2cWriteBuffer(addr, temp, false);
-        return pins.i2cReadBuffer(addr, len, false);
-    }
-    function readNumber(addr: number, register: number, fmt: NumberFormat = NumberFormat.UInt8LE): number {
-        let temp = pins.createBuffer(1);
-        temp[0] = register;
-        pins.i2cWriteBuffer(addr, temp, false);
-        return pins.i2cReadNumber(addr, fmt, false);
-    }
-}
-
 //% weight=100 color=#000000 icon="\uf06e" block="Scroll:Bit"
 namespace scrollbit {
     const I2C_ADDR: number = 0x74
@@ -44,6 +15,60 @@ namespace scrollbit {
 
     const COLS: number = 17
     const ROWS: number = 7
+
+    const ARROWOFFSET: number = 40
+
+    const ICONS: string[] = [
+        "Heart",
+        "SmallH",
+        "Yes",
+        "No",
+        "Happy",
+        "Sad",
+        "Confus",
+        "Angry",
+        "Asleep",
+        "Surpri",
+        "Silly",
+        "Fabulo",
+        "Meh",
+        "TShirt",
+        "Roller",
+        "Duck",
+        "House",
+        "Tortoi",
+        "Butter",
+        "StickF",
+        "Ghost",
+        "Sword",
+        "Giraff",
+        "Skull",
+        "Umbrel",
+        "Snake",
+        "Rabbit",
+        "Cow",
+        "Quarte",
+        "EightN",
+        "Pitchf",
+        "Target",
+        "Triang",
+        "LeftTr",
+        "Chessb",
+        "Diamon",
+        "SmallD",
+        "Square",
+        "SmallS",
+        "Scisso",
+    
+        "North",
+        "NorthE",
+        "East",
+        "SouthE",
+        "South",
+        "SouthW",
+        "West",
+        "NorthW"
+    ]
 
     const GAMMA: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
@@ -64,6 +89,277 @@ namespace scrollbit {
 
     let buf: Buffer = pins.createBuffer(144)
     let frame: number = 0
+
+    /**
+     * Scroll text across scroll:bit
+     * @param text - text to scroll
+     * @param brightness - brightness to set (0-255)
+     * @param delay - additional delay in milliseconds (0-100)
+     */
+    //% blockId=scrollbit_scroll_text 
+    //% block="scroll %text| at brightness %brightness| with delay (ms) %delay"
+    //% brightness.min=0 brightness.max=255 brightness.defl=128
+    //% delay.min=0 delay.max=100 delay.defl=50
+    export function scrollText(text: string, brightness: number = 128, delay: number = 50) {
+        text = tokenize(text)
+        let len: number = measureText(text)
+        for (let col: number = 0; col < len + COLS; col++){
+            clear()
+            _drawText(text, -col + COLS, 1, brightness)
+            show()
+            if (delay > 0) {
+                control.waitMicros(delay * 1000)
+            }    
+        }
+    }
+
+    function tokenize(text: string): string {
+        let result: string = ""
+        let icon: string = ""
+
+        for (let x = 0; x < text.length; x++){
+            let char: string = text.charAt(x)
+            if (char == "}" && icon.length > 0) {
+                let index: number = ICONS.indexOf(icon.substr(1,6))
+                icon += char
+
+                if (index > -1) {
+                    icon = String.fromCharCode(DAL.MICROBIT_FONT_ASCII_END + 1 + index)
+                }
+
+                result += icon
+                icon = ""
+                continue
+            }
+            if (char == "{" || icon.length > 0) {
+                icon += char
+                continue
+            }
+            result += char
+        }
+
+        return result
+    }
+
+    /**
+     * Display an icon on scroll:bit
+     * @param icon - icon to display
+     * @param col - column to set (0-16)
+     * @param row - row to set (0-6)
+     * @param brightness - brightness to set (0-255)
+     */
+    //% blockId=scrollbit_set_icon
+    //% block="display icon %icon| at col %col| row %row| with brightness %brightness"
+    //% col.min=0 col.max=16
+    //% row.min=0 row.max=6
+    //% brightness.min=0 brightness.max=255 brightness.defl=128
+    export function setIcon(icon: IconNames, col: number, row: number, brightness: number = 128): void {
+        let image: Image = images.iconImage(icon)
+        setImage(image, col, row, brightness)
+    }
+
+    /**
+     * Display an arrow on scroll:bit
+     * @param arrow - arrow to display
+     * @param col - column to set (0-16)
+     * @param row - row to set (0-6)
+     * @param brightness - brightness to set (0-255)
+     */
+    //% blockId=scrollbit_set_arrow
+    //% block="display arrow %arrow| at col %col| row %row| with brightness %brightness"
+    //% col.min=0 col.max=16
+    //% row.min=0 row.max=6
+    //% brightness.min=0 brightness.max=255 brightness.defl=128
+    export function setArrow(arrow: ArrowNames, col: number, row: number, brightness: number = 128): void {
+        let image: Image = images.arrowImage(arrow)
+        setImage(image, col, row, brightness)
+    }
+
+    function setImage(image: Image, col: number, row: number, brightness: number = 128): void {
+        for (let c_row = 0; c_row < 5; c_row++) {
+            for (let c_col = 0; c_col < 5; c_col++) {
+                if (image.pixelBrightness(c_col, c_row)) {
+                    setPixel(col + c_col, row + c_row, brightness)
+                }
+            }
+        }
+    }
+
+    /**
+     * Set a single pixel on scroll:bit
+     * @param col - column to set (0-16)
+     * @param row - row to set (0-6)
+     * @param brightness - brightness to set (0-255)
+     */
+    //% blockId=scrollbit_set_pixel
+    //% block="set pixel| at col %col| row %row| to %brightness"
+    //% col.min=0 col.max=16
+    //% row.min=0 row.max=6
+    //% brightness.min=0 brightness.max=255 brightness.defl=128
+    export function setPixel(col: number, row: number, brightness: number=128): void {
+        buf[pixelAddr(col, row)] = Math.clamp(0, 255, brightness)
+    }
+
+    /**
+     * Update scroll:bit,
+     * update the display with your pretty pixels
+     */
+    //% blockId=scrollbit_show
+    //% block="display your changes"
+    export function show(): void {
+        let corrected_buf: Buffer = pins.createBuffer(144)
+
+        for (let x = 0; x < buf.length; x++) {
+            corrected_buf[x] = GAMMA[buf[x]]
+        }
+
+        smbus.writeByte(I2C_ADDR, REG_BANK, frame)
+        smbus.writeBuffer(I2C_ADDR, REG_COLOR, corrected_buf)
+        smbus.writeByte(I2C_ADDR, REG_BANK, BANK_CONFIG)
+        smbus.writeByte(I2C_ADDR, REG_FRAME, frame)
+
+        frame = frame == 0 ? 1 : 0
+    }
+
+    /**
+     * Clear scroll:bit,
+     * sets all pixels to 0, but does not update display
+     */
+    //% blockId=scrollbit_clear
+    //% block="clear scroll:bit"
+    //% icon=""
+    export function clear(): void {
+        buf.fill(0)
+    }
+
+    /**
+     * Return the width (number of cols) of scroll:bit
+     */
+    //% blockId=scrollbit_cols
+    //% block="number of columns"
+    //% color=#444444
+    //% icon=""
+    export function cols(): number {
+        return COLS
+    }
+
+    /**
+     * Return the height (number of rows) of scroll:bit
+     */
+    //% blockId=scrollbit_rows
+    //% block="number of rows"
+    //% color=#444444
+    //% icon=""
+    export function rows(): number {
+        return ROWS
+    }
+
+    /**
+     * Get a single pixel on scroll:bit,
+     * returns a brightness value (0-255)
+     * @param col - column to get (0-16)
+     * @param row - row to get (0-6)
+     */
+    //% blockId=scrollbit_get_pixel icon="\uf0eb"
+    //% block="get pixel| at col %col| row %row"
+    //% advanced color=#554444
+    export function getPixel(col: number, row: number): number {
+        return buf[pixelAddr(col, row)]
+    }
+
+    /**
+     * Measure text, returns a length in pixels
+     * @param text - text to measure
+     */
+    //% blockId=scrollbit_measure_text
+    //% block="get length of %text in pixels"
+    //% advanced color=#554444
+    export function measureText(text: string): number {
+        let len: number = 0
+        for (let x: number = 0; x < text.length; x++){
+            len += charWidth(text.charAt(x)) + 1
+        }
+        return len
+    }
+
+    /**
+     * Draw a single alphanumeric character.
+     * @param col - column position (0-16)
+     * @param row - row position (0-6)
+     * @param char - character to display
+     * @param brightness - brightness (0-255)
+     */
+    export function drawChar(char: string, col: number, row: number, brightness: number = 128): void {
+        if (char.charCodeAt(0) > DAL.MICROBIT_FONT_ASCII_END + ARROWOFFSET) {
+            setArrow(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - ARROWOFFSET - 1, col, row, brightness)
+            return;
+        }
+        if (char.charCodeAt(0) > DAL.MICROBIT_FONT_ASCII_END) {
+            setIcon(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - 1, col, row, brightness)
+            return;
+        }
+        let data: Buffer = getChar(char)
+        for (let c_row = 0; c_row < 5; c_row++) {
+            for (let c_col = 0; c_col < 5; c_col++) {
+                if ((data[c_row] & (1 << (4 - c_col))) > 0) {
+                    setPixel(col + c_col, row + c_row, brightness)
+                }
+            }
+        }
+    }
+
+    /**
+     * Draw text on scroll:bit
+     * @param col - column to set (0-16)
+     * @param row - row to set (0-6)
+     * @param text - text to show
+     * @param brightness - brightness to set (0-255)
+     */
+    //% blockId=scrollbit_draw_text
+    //% block="draw %text| at col %col| row %row| with brightness %brightness"
+    //% advanced color=#220000
+    //% col.min=0 col.max=16
+    //% row.min=0 row.max=6
+    //% brightness.min=0 brightness.max=255 brightness.defl=128
+    export function drawText(text: string, col: number, row: number, brightness: number = 128): void {
+        text = tokenize(text)
+        _drawText(text, col, row, brightness)
+    }
+    export function _drawText(text: string, col: number, row: number, brightness: number = 128): void {
+        let offset_col: number = 0
+        for (let x: number = 0; x < text.length; x++){
+            let width:  number = charWidth(text.charAt(x))
+            if (col + offset_col >= COLS) {
+                return
+            }
+            if (col + offset_col + width < 0) {
+                offset_col += width + 1
+                continue
+            }
+            drawChar(text.charAt(x), col + offset_col, row, brightness)
+            offset_col += width + 1
+        }
+    }
+
+    /**
+     * TODO: Get some GUI/UX magic for this
+     * Set all the pixels on scroll:bit using
+     * a text string. Brightness = charCode.
+     * eg ' ' = 32, 'z' = 122
+     */
+    //% blockId=scrollbit_show_leds
+    //% block="display LEDs from text %text"
+    //% advanced color=#220000
+    export function showLeds(leds: string): void {
+        leds = leds.substr(0, COLS * ROWS)
+        for (let x = 0; x < leds.length; x++) {
+            let brightness = leds.charCodeAt(x);
+            let col = x % COLS
+            let row = (x - col) / COLS
+            setPixel(col, row, brightness)
+        }
+        show()
+    }
 
     function pixelAddr(col: number, row: number): number {
         row = 7 - (row + 1)
@@ -99,7 +395,11 @@ namespace scrollbit {
     }
 
     function charWidth(character: string): number {
-        return getCharWidth(character.charCodeAt(0))
+        let charcode: number = character.charCodeAt(0)
+        if (charcode > DAL.MICROBIT_FONT_ASCII_END) {
+            return 5
+        }
+        return getCharWidth(charcode)
     }
 
     /**
@@ -126,179 +426,34 @@ namespace scrollbit {
         smbus.writeByte(I2C_ADDR, REG_BANK, 1)
         smbus.writeBuffer(I2C_ADDR, 0, enable)
     }
+}
 
-    /**
-     * TODO: Get some GUI/UX magic for this
-     * Set all the pixels on scroll:bit using
-     * a text string. Brightness = charCode.
-     * eg ' ' = 32, 'z' = 122
-     */
-    //% block
-    export function showLeds(leds: string): void {
-        leds = leds.substr(0, COLS * ROWS)
-        for (let x = 0; x < leds.length; x++) {
-            let brightness = leds.charCodeAt(x);
-            let col = x % COLS
-            let row = (x - col) / COLS
-            setPixel(col, row, brightness)
+namespace smbus {
+    export function writeByte(addr: number, register: number, value: number): void {
+        let temp = pins.createBuffer(2);
+        temp[0] = register;
+        temp[1] = value;
+        pins.i2cWriteBuffer(addr, temp, false);
+    }
+    export function writeBuffer(addr: number, register: number, value: Buffer): void {
+        let temp = pins.createBuffer(value.length + 1);
+        temp[0] = register;
+        for (let x = 0; x < value.length; x++) {
+            temp[x + 1] = value[x];
         }
-        show()
+        pins.i2cWriteBuffer(addr, temp, false);
     }
-
-    /**
-     * Set a single pixel on scroll:bit
-     * @param col - column to set (0-16)
-     * @param row - row to set (0-6)
-     * @param brightness - brightness to set (0-255)
-     */
-    //% blockId=scrollbit_set_pixel
-    //% block="set pixel|at col %col| row %row|to %brightness"
-    //% col.min=0 col.max=16
-    //% row.min=0 row.max=6
-    //% brightness.min=0 brightness.max=255
-    export function setPixel(col: number, row: number, brightness: number): void {
-        buf[pixelAddr(col, row)] = Math.clamp(0, 255, brightness)
+    export function readBuffer(addr: number, register: number, len: number): Buffer {
+        let temp = pins.createBuffer(1);
+        temp[0] = register;
+        pins.i2cWriteBuffer(addr, temp, false);
+        return pins.i2cReadBuffer(addr, len, false);
     }
-
-    /**
-     * Get a single pixel on scroll:bit,
-     * returns a brightness value (0-255)
-     * @param col - column to get (0-16)
-     * @param row - row to get (0-6)
-     */
-    //% blockId=scrollbit_get_pixel icon="\uf0eb"
-    //% block="get pixel|at col %col| row %row"
-    export function getPixel(col: number, row: number): number {
-        return buf[pixelAddr(col, row)]
-    }
-
-    /**
-     * Draw a single alphanumeric character.
-     * @param col - column position (0-16)
-     * @param row - row position (0-6)
-     * @param char - character to display
-     * @param brightness - brightness (0-255)
-     */
-    //% block
-    //% col.min=0 col.max=16
-    //% row.min=0 row.max=6
-    //% brightness.min=0 brightness.max=255
-    export function drawChar(col: number, row: number, char: string, brightness: number=128) {
-        let data: Buffer = getChar(char)
-        for (let c_row = 0; c_row < 5; c_row++) {
-            for (let c_col = 0; c_col < 5; c_col++) {
-                if ((data[c_row] & (1 << (4 - c_col))) > 0) {
-                    setPixel(col + c_col, row + c_row, brightness)
-                }
-            }
-        }
-    }
-
-    /**
-     * Draw text on scroll:bit
-     * @param col - column to set (0-16)
-     * @param row - row to set (0-6)
-     * @param text - text to show
-     * @param brightness - brightness to set (0-255)
-     */
-    //% block
-    //% col.min=0 col.max=16
-    //% row.min=0 row.max=6
-    //% brightness.min=0 brightness.max=255
-    export function drawText(col: number, row: number, text: string, brightness: number=128) {
-        let offset_col: number = 0
-        for (let x: number = 0; x < text.length; x++){
-            let width:  number = charWidth(text.charAt(x))
-            if (col + offset_col >= COLS) {
-                return
-            }
-            if (col + offset_col + width < 0) {
-                offset_col += width + 1
-                continue
-            }
-            drawChar(col + offset_col, row, text.charAt(x), brightness)
-            offset_col += width + 1
-        }
-    }
-
-    /**
-     * Scroll text across scroll:bit
-     * @param text - text to scroll
-     * @param brightness - brightness to set (0-255)
-     * @param delay - additional delay in milliseconds (0-100)
-     */
-    //% block
-    //% brightness.min=0 brightness.max=255
-    //% delay.min=0 delay.max=100
-    export function scrollText(text: string, brightness: number=128, delay: number=50) {
-        let len: number = measureText(text)
-        for (let col: number = 0; col < len + COLS; col++){
-            clear()
-            drawText(-col + COLS, 1, text, brightness)
-            show()
-            if (delay > 0) {
-                control.waitMicros(delay * 1000)
-            }    
-        }
-
-    }
-
-    /**
-     * Measure text, returns a length in pixels
-     * @param text - text to measure
-     */
-    //% block advanced
-    export function measureText(text: string): number {
-        let len: number = 0
-        for (let x: number = 0; x < text.length; x++){
-            len += charWidth(text.charAt(x)) + 1
-        }
-        return len
-    }
-
-    /**
-     * Clear scroll:bit,
-     * sets all pixels to 0, but does not update display
-     */
-    //% block
-    export function clear(): void {
-        buf.fill(0)
-    }
-
-    /**
-     * Update scroll:bit,
-     * update the display with your pretty pixels
-     */
-    //% block
-    export function show(): void {
-        let corrected_buf: Buffer = pins.createBuffer(144)
-
-        for (let x = 0; x < buf.length; x++) {
-            corrected_buf[x] = GAMMA[buf[x]]
-        }
-
-        smbus.writeByte(I2C_ADDR, REG_BANK, frame)
-        smbus.writeBuffer(I2C_ADDR, REG_COLOR, corrected_buf)
-        smbus.writeByte(I2C_ADDR, REG_BANK, BANK_CONFIG)
-        smbus.writeByte(I2C_ADDR, REG_FRAME, frame)
-
-        frame = frame == 0 ? 1 : 0
-    }
-
-    /**
-     * Return the width (number of cols) of scroll:bit
-     */
-    //% block color=#444444
-    export function cols(): number {
-        return COLS
-    }
-
-    /**
-     * Return the height (number of rows) of scroll:bit
-     */
-    //% block color=#444444
-    export function rows(): number {
-        return ROWS
+    function readNumber(addr: number, register: number, fmt: NumberFormat = NumberFormat.UInt8LE): number {
+        let temp = pins.createBuffer(1);
+        temp[0] = register;
+        pins.i2cWriteBuffer(addr, temp, false);
+        return pins.i2cReadNumber(addr, fmt, false);
     }
 }
 
--- test.ts
+++ test.ts
@@ -1,9 +1,119 @@
 // tests go here; this will not be compiled when this package is used as a library
 
+/*
+basic.forever(() => {
+    scrollbit.scrollText("{Heart}")
+    scrollbit.scrollText("{SmallHeart}")
+    scrollbit.scrollText("{Yes}")
+    scrollbit.scrollText("{No}")
+    scrollbit.scrollText("{Happy}")
+    scrollbit.scrollText("{Sad}")
+    scrollbit.scrollText("{Confused}")
+    scrollbit.scrollText("{Angry}")
+    scrollbit.scrollText("{Asleep}")
+    scrollbit.scrollText("{Surprised}")
+    scrollbit.scrollText("{Silly}")
+    scrollbit.scrollText("{Fabulous}")
+    scrollbit.scrollText("{Meh}")
+    scrollbit.scrollText("{TShirt}")
+    scrollbit.scrollText("{Rollerskate}")
+    scrollbit.scrollText("{Duck}")
+    scrollbit.scrollText("{House}")
+    scrollbit.scrollText("{Tortoise}")
+    scrollbit.scrollText("{Butterfly}")
+    scrollbit.scrollText("{StickFigure}")
+    scrollbit.scrollText("{Ghost}")
+    scrollbit.scrollText("{Sword}")
+    scrollbit.scrollText("{Giraffe}")
+    scrollbit.scrollText("{Skull}")
+    scrollbit.scrollText("{Umbrella}")
+    scrollbit.scrollText("{Snake}")
+    scrollbit.scrollText("{Rabbit}")
+    scrollbit.scrollText("{Cow}")
+    scrollbit.scrollText("{QuarterNote}")
+    scrollbit.scrollText("{EightNote}")
+    scrollbit.scrollText("{Pitchfork}")
+    scrollbit.scrollText("{Target}")
+    scrollbit.scrollText("{Triangle}")
+    scrollbit.scrollText("{LeftTriangle}")
+    scrollbit.scrollText("{Chessboard}")
+    scrollbit.scrollText("{Diamond}")
+    scrollbit.scrollText("{SmallDiamond}")
+    scrollbit.scrollText("{Square}")
+    scrollbit.scrollText("{SmallSquare}")
+    scrollbit.scrollText("{Scissors}")
+    scrollbit.scrollText("{North}")
+    scrollbit.scrollText("{NorthEast}")
+    scrollbit.scrollText("{East}")
+    scrollbit.scrollText("{SouthEast}")
+    scrollbit.scrollText("{South}")
+    scrollbit.scrollText("{SouthWest}")
+    scrollbit.scrollText("{West}")
+    scrollbit.scrollText("{NorthW}")
+})
+*/
+
+/*
+
 basic.showIcon(IconNames.Heart)
+
+let brightness: number = 128
+
+for (let col: number = 0; col < scrollbit.cols(); col++){
+    for (let row: number = 0; row < scrollbit.rows(); row++){
+        let offset: number = (row * scrollbit.cols()) + col
+        let br: number = (brightness + 25) - ((offset * brightness) / 119)
+        br = Math.max(0, br)
+        if (offset > 117) br = 0
+        scrollbit.setPixel(col, row, br)
+    }
+}
+
+scrollbit.show()
+*/
+
+/*
+// Test Jig Code
+
+basic.forever(function(){
+
+    for (let col: number = 0; col < scrollbit.cols(); col++){
+        for (let row: number = 0; row < scrollbit.rows(); row++){
+            let offset: number = (col * scrollbit.cols()) + row
+            scrollbit.setPixel(col, row, 64 * (offset % 2))
+        }
+    }
+    scrollbit.show()
+    control.waitMicros(1000000)
+    for (let col: number = 0; col < scrollbit.cols(); col++){
+        for (let row: number = 0; row < scrollbit.rows(); row++){
+            let offset: number = (col * scrollbit.cols()) + row
+            scrollbit.setPixel(col, row, 64 * ((offset + 1) % 2))
+        }
+    }
+    scrollbit.show()
+    control.waitMicros(1000000)
+    for (let col: number = 0; col < scrollbit.cols(); col++){
+        for (let row: number = 0; row < scrollbit.rows(); row++){
+            let offset: number = (col * scrollbit.cols()) + row
+            scrollbit.setPixel(col, row, 64)
+        }
+    }
+    scrollbit.show()
+    control.waitMicros(1000000)
+    scrollbit.clear()    
+    scrollbit.show()
+    control.waitMicros(1000000)
+
+})
+*/
+
+/*
+
 scrollbit.scrollText("Yarrrrrrrrrrrrrrrrrr")
 scrollbit.scrollText("The quick, brown fox jumped over the lazy dog! How slow does this get when displaying a really long sentence? I wonder. Let's see how far we can push this thing.", 128, 0)
 scrollbit.scrollText("Hello World! How are you today?", 128)
 scrollbit.scrollText("Super duper scrollalicious!", 128)
 scrollbit.setPixel(0,0,128)
 scrollbit.show()
+*/
